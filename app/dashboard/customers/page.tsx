e246bd3234ab771ec6c522b57c5941d66ff9b73e
sqy-harshil <harshil.patel@squareyards.co.in>
2023-11-26 22:40:59 +0530

    Basic NextJS app

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+function CustomerPage(){
+    return <p>This is the customer's content within the Dashboard</p>
+}
+
+export default CustomerPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+function InvoicesPage(){
+    return <h1>This is the Invoice's content within the Dashboard</h1>
+}
+
+export default InvoicesPage
